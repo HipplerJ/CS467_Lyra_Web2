9ac319d38ec3780e2ac0d4eb2d3c88e7931e235b
Evin Colignon <ecoligno@gmail.com>
2018-07-28 21:37:31 -0700

    Add script that opens json file and prints contents to post data screen

--- public/js/draw-results.js
+++ public/js/draw-results.js
@@ -1,8 +1,34 @@
+// set up arbor.js particle system and renderer
 var sys = arbor.ParticleSystem(1000, 400,1);
 sys.parameters({gravity:true});
 sys.renderer = Renderer("#viewport") ;
-var node1 = sys.addNode('url-1',{'color':'red','shape':'rectangle','label':'oregonstate.edu'});
-var node2 = sys.addNode('url-2',{'color':'green','shape':'rectangle','label':'facebook.com'});
-var node3 = sys.addNode('url-3',{'color':'blue','shape':'rectangle','label':'google.com'});
-sys.addEdge(node1, node2);
-sys.addEdge(node1, node3);
+
+// draw/test after this element
+var test_data = $('#testdata');
+
+// FIXME test: open dummy json file with .getJSON and display contents using $.getJSON
+$.getJSON('json/test-data.json', function(data){
+
+  // log to console
+  console.log(data); //WORKS
+
+  // print out json file data
+  // reference: https://api.jquery.com/jquery.getjson/
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + JSON.stringify(key) + "'>" + JSON.stringify(val) + "</li>" );
+  });
+
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( test_data );
+
+});
+
+// FIXME first example - draw 3 node graph using arbor.js
+// var node1 = sys.addNode('url-1',{'color':'red','shape':'rectangle','label':'oregonstate.edu'});
+// var node2 = sys.addNode('url-2',{'color':'green','shape':'rectangle','label':'facebook.com'});
+// var node3 = sys.addNode('url-3',{'color':'blue','shape':'rectangle','label':'google.com'});
+// sys.addEdge(node1, node2);
+// sys.addEdge(node1, node3);
