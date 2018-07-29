face01f53bcef12847c00e4cca94fc8a88259ac6
Evin Colignon <ecoligno@gmail.com>
2018-07-28 22:08:20 -0700

    Make results page show posted data, dummy graph example, and dummy JSON file dump

--- public/js/draw-results.js
+++ public/js/draw-results.js
@@ -27,8 +27,8 @@ $.getJSON('json/test-data.json', function(data){
 });
 
 // FIXME first example - draw 3 node graph using arbor.js
-// var node1 = sys.addNode('url-1',{'color':'red','shape':'rectangle','label':'oregonstate.edu'});
-// var node2 = sys.addNode('url-2',{'color':'green','shape':'rectangle','label':'facebook.com'});
-// var node3 = sys.addNode('url-3',{'color':'blue','shape':'rectangle','label':'google.com'});
-// sys.addEdge(node1, node2);
-// sys.addEdge(node1, node3);
+var node1 = sys.addNode('url-1',{'color':'red','shape':'rectangle','label':'oregonstate.edu'});
+var node2 = sys.addNode('url-2',{'color':'green','shape':'rectangle','label':'facebook.com'});
+var node3 = sys.addNode('url-3',{'color':'blue','shape':'rectangle','label':'google.com'});
+sys.addEdge(node1, node2);
+sys.addEdge(node1, node3);
