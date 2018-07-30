ec188b662f303385ec1e705598adaeb865222af1
Evin Colignon <ecoligno@gmail.com>
2018-07-29 19:29:14 -0700

    Draw tree in visualization page using sample JSON file

--- public/js/draw-results.js
+++ public/js/draw-results.js
@@ -3,10 +3,21 @@ var sys = arbor.ParticleSystem(1000, 400,1);
 sys.parameters({gravity:true});
 sys.renderer = Renderer("#viewport") ;
 
-// draw/test after this element
-var test_data = $('#testdata');
+// variables
+var test_data = $('#testdata');  // draw/test after this element
+var canvas = $('canvas');  // html canvas element
 
-// FIXME test: open dummy json file with .getJSON and display contents using $.getJSON
+// resize canvas to window size
+canvas.width('100%');
+canvas.height('auto');
+
+// window resize listener - resize canvas to window size
+$(window).resize(function(){
+  canvas.width('100%');
+  canvas.height('auto');
+})
+
+// test: open dummy json file with .getJSON and display contents using $.getJSON
 $.getJSON('json/test-data.json', function(data){
 
   // log to console
@@ -24,25 +35,24 @@ $.getJSON('json/test-data.json', function(data){
     html: items.join( "" )
   }).appendTo( test_data );
 
+
+  // TODO draw the data
+  sys.graft({nodes:data.nodes, edges:data.edges});
+
+
 });
 
+
+
+
 // FIXME first example - draw 3 node graph using arbor.js
-var node1 = sys.addNode('url-1',{'color':'red','shape':'rectangle','label':'oregonstate.edu'});
-var node2 = sys.addNode('url-2',{'color':'green','shape':'rectangle','label':'facebook.com'});
-var node3 = sys.addNode('url-3',{'color':'blue','shape':'rectangle','label':'google.com'});
-sys.addEdge(node1, node2);
-sys.addEdge(node1, node3);
-
-// resize canvas to window size
-var canvas = $('canvas');
-canvas.width('100%');
-canvas.height('auto');
+// var node1 = sys.addNode('url-1',{'color':'red','shape':'rectangle','label':'oregonstate.edu'});
+// var node2 = sys.addNode('url-2',{'color':'green','shape':'rectangle','label':'facebook.com'});
+// var node3 = sys.addNode('url-3',{'color':'blue','shape':'rectangle','label':'google.com'});
+// sys.addEdge(node1, node2);
+// sys.addEdge(node1, node3);
 
 
-// window resize listener - resize canvas to window size
-$(window).resize(function(){
-  canvas.width('100%');
-  canvas.height('auto');
-})
+
 
 // particleSystem.screenSize(w,h);
