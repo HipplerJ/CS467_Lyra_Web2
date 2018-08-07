0e23b96a293b9b820b2d61a62883ccc80ea606c8
HipplerJ <32230179+HipplerJ@users.noreply.github.com>
2018-08-07 01:47:04 -0500

    Changed Host to external server 0.0.0.0 instead of localhost.  Now able to host on Flip Environment

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def results():
     return render_template('results.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=7777, threaded=True)
+    app.run(host="0.0.0.0", debug=True, port=7777, threaded=True)
